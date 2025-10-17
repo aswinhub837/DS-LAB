f4ce9a85b7a5a2666c4585325ed15b10ec331266
aswinhub837 <aswinbiju306@gmail.com>
2025-10-17 14:33:08 +0530

    Add files via upload

---
+++ BFT.C
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int a[20][20],q[20],visited[20],n,i,j,f=0,r=-1; // a[20][20]: adjacency matrix
+void bfs(int start)
+{
+q[++r]=start;
+visited[start]=1;
+for(i=1;i<=n;i++)
+{
+if(a[start][i]&&!visited[i])
+{
+q[++r]=i;
+visited[i]=1;
+printf("%d ",q[r]);
+bfs(q[f++]);
+}
+}
+}
+void main()
+{
+
+int v;
+clrscr();
+printf("\n Enter the number of vertices:");
+scanf("% d",&n);
+for(i=1;i<n;i++)
+{
+q[i]=0;
+visited[i]=0;
+}
+printf("\n Enter values in adjacency matrix:\n");
+//Elements (Os or 1s) are entered as adjacency matrix
+for(i=1;i<=n;i++)
+{
+for(j=1;j<=n;j++)
+{
+scanf("% d",&a[i][j]);
+}
+}
+printf("\n Enter the starting vertex:");
+scanf("% d", &v);
+printf("\n The node which are reachable are:\n");
+printf("% d ",v);
+bfs(v);
+getch();
+}
